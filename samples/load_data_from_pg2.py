331054854694c3b6dbd3998bd0d8b749746c21ab
Ryan Zhang <rzhang@redhat.com>
2022-01-19 16:55:46 +0800

    Add new Split_Idx_d 脚本和增加数据库表说明文件

--- samples/load_data_from_pg2.py
+++ samples/load_data_from_pg2.py
@@ -1,23 +1,29 @@
 # -*- coding: UTF-8 -*-
 
 import pandas as pd
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import psycopg2
+import time
+
 
 # pd.set_option('expand_fram_repr', False)
 
 #导入数据
 # postgres config
-postgres_host = "192.168.2.13"               # 数据库地址
+# postgres_host = "192.168.2.20"               # prod 数据库地址
+# postgres_host = "192.168.2.13"               # staging 数据库地址
+postgres_host = "postgresql"               # dev 数据库地址
 postgres_port = "5432"       # 数据库端口
 postgres_user = "user"              # 数据库用户名
 postgres_password = "password"      # 数据库密码
 postgres_datebase = "market"      # 数据库名字
-postgres_table1 = "stock.equity"           #数据库中的表的名字
+postgres_table1 = "stock.mkt_equ_day"           #数据库中的表的名字
 
 # connection string
 conn_string = "host=" + postgres_host + " port=" + postgres_port + " dbname=" + postgres_datebase + \
                 " user=" + postgres_user + " password=" + postgres_password
+start_time = time.time()
+
 conn = psycopg2.connect(conn_string)
 
 sql_command1 = "select * from " + postgres_table1
@@ -31,6 +37,8 @@ except:
 
 if data1.shape[0] == 0:
     print("there is no data in !")
-else:
-    print(data1)
+
+print(type (data1['trade_date'][0]))
+
+print ("Executed Time(s):-", time.time()-start_time)
 
